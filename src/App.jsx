e8f2fa64504d33594148ac94ea398da935fed2ab
Artem <suleymanov2007artem@gmail.com>
2025-01-25 22:33:44 +0200

    add css styles contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
-  const [contacts, getContacts] = useState([
+  const [contacts, setContacts] = useState([
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
 
+  const deleteContact = (contactId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    );
+  };
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} onDeleteContact={deleteContact} />
     </div>
   );
 };
